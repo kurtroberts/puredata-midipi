6962926ae309d20a6c7d92896a3485ecab1e3b72
Kurt Roberts <kurt.roberts@gmail.com>
2015-02-15 15:08:53 -0500

    updating startup scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+	shell = require('gulp-shell'),
 	paths = {
 		puredata: './puredata/**',
 		pdTarget: '/home/pi/pd-patches/'
@@ -9,3 +10,7 @@ gulp.task('puredata', function () {
 	return gulp.src(paths.puredata)
 			.pipe(gulp.dest(paths.pdTarget));
 });
+
+gulp.task('midiports', shell.task(['cat /dev/sndstat']));
+
+gulp.task('interactive', shell.task(['pd-extended -midiindev 0,1 -midioutdev 0,1 ']));
--- pi-config/rc.local/start_pd.sh
+++ pi-config/rc.local/start_pd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # This script will start Pd in nogui
 # mode and will open the patch
-# named "nicorette.pd"
 echo "Starting Pd..."
-pd -nogui -midiindev 0,1 -midioutdev 0,1 /home/pi/pd-patches/defaults.pd &
+pd-extended -nogui -midiindev 0,1 -midioutdev 0,1 /home/pi/pd-patches/default.pd &
